e9c545f031fbff123b61da6c1219b82b64db38b2
Haroon Anwar Padhyar <haroon.padhyar@gmail.com>
2019-03-24 18:09:32 +0300

    KahaDB Persistence.

--- web/src/main/java/com/mine/jms/config/JmsConfig.java
+++ web/src/main/java/com/mine/jms/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.mine.jms.config;
 
+import java.io.File;
 import java.net.InetAddress;
 import java.util.Collections;
 import javax.jms.ConnectionFactory;
@@ -18,6 +19,8 @@ import org.apache.activemq.store.PersistenceAdapter;
 import org.apache.activemq.store.jdbc.JDBCPersistenceAdapter;
 import org.apache.activemq.store.jdbc.LeaseDatabaseLocker;
 import org.apache.activemq.store.jdbc.adapter.MySqlJDBCAdapter;
+import org.apache.activemq.store.jdbc.adapter.TransactJDBCAdapter;
+import org.apache.activemq.store.kahadb.KahaDBPersistenceAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.connection.JmsTransactionManager;
@@ -30,16 +33,43 @@ import org.springframework.transaction.PlatformTransactionManager;
  *         Created on 3/11/19 6:44 PM.
  */
 public class JmsConfig {
-  private static final String BROKER_URL ="tcp://localhost:6616";
+//  private static final String BROKER_URL ="tcp://localhost:6616";
+  private static final String BROKER_URL ="vm://localhost"; // will use KahaDB persistence
+//  private static final String BROKER_URL ="vm://broker1?marshal=false&broker.persistent=false";
+
+
+  /**
+   * vm://broker1?marshal=false&broker.persistent=false
+   *
+   *  **Be careful with embedded brokers**
+   *
+   * If you are using the VM transport and wish to explicitly configure an Embedded Broker there is a chance that you
+   * could create the JMS connections first before the broker starts up.
+   * Currently ActiveMQ will auto-create a broker if you use the VM transport and there is not one already configured.
+   * (In 5.2 it is possible to use the waitForStart and create=false options for the connection uri)
+   *
+   * So to work around this if you are using Spring you may wish to use the depends-on attribute so that your
+   * JMS ConnectionFactory depends on the embedded broker to avoid this happening. e.g.
+   *
+   * <bean id="broker" class="org.apache.activemq.xbean.BrokerFactoryBean">
+   *     <property name="config" value="classpath:org/apache/activemq/xbean/activemq.xml" />
+   *     <property name="start" value="true" />
+   *   </bean>
+   *
+   *   <bean id="connectionFactory" class="org.apache.activemq.ActiveMQConnectionFactory" depends-on="broker">
+   *     <property name="brokerURL" value="vm://localhost"/>
+   *   </bean>
+   */
 
   @Autowired
   private DataSource dataSource;
 
+  // Comment the broker configuration to use implicit broker (in case of VM transport) that will be created automatically.
   @Bean
   public BrokerService broker() throws Exception{
     BrokerService broker = new BrokerService();
     broker.addConnector(BROKER_URL);
-    broker.setPersistent(true);
+    broker.setPersistent(true); // By default KahaDB and location is tomcat\bin\activemq-data\localhost\KahaDB
     broker.setBrokerName("Broker: "+ InetAddress.getLocalHost().getHostName());
     broker.setPersistenceAdapter(persistenceAdapter());
     broker.setUseJmx(true);
@@ -65,6 +95,13 @@ public class JmsConfig {
     return policyMap;
   }
 
+  // KahaDB
+  private PersistenceAdapter persistenceAdapter() throws Exception{
+    KahaDBPersistenceAdapter persistenceAdapter = new KahaDBPersistenceAdapter();
+    persistenceAdapter.setDirectory(new File("C:\\Projects\\ehis-projects\\configcomponent\\local\\tomcat\\bin\\activemq-data\\localhost\\KahaDB"));
+    return persistenceAdapter;
+  }
+
 //  SQL Server
 //  private PersistenceAdapter persistenceAdapter() throws Exception{
 //    JDBCPersistenceAdapter persistenceAdapter = new JDBCPersistenceAdapter();
@@ -79,23 +116,24 @@ public class JmsConfig {
 //  }
 
 //  MySQL
-  private PersistenceAdapter persistenceAdapter() throws Exception{
-    JDBCPersistenceAdapter persistenceAdapter = new JDBCPersistenceAdapter();
-    persistenceAdapter.setDataSource(dataSource);
-    LeaseDatabaseLocker locker = new LeaseDatabaseLocker();
-    locker.setLockAcquireSleepInterval(1000L);
-    persistenceAdapter.setLocker(locker);
-    persistenceAdapter.setLockKeepAlivePeriod(5000L);
-    persistenceAdapter.setCreateTablesOnStartup(true);
-    persistenceAdapter.setAdapter(new MySqlJDBCAdapter());
-    return persistenceAdapter;
-  }
+//  private PersistenceAdapter persistenceAdapter() throws Exception{
+//    JDBCPersistenceAdapter persistenceAdapter = new JDBCPersistenceAdapter();
+//    persistenceAdapter.setDataSource(dataSource);
+//    LeaseDatabaseLocker locker = new LeaseDatabaseLocker();
+//    locker.setLockAcquireSleepInterval(1000L);
+//    persistenceAdapter.setLocker(locker);
+//    persistenceAdapter.setLockKeepAlivePeriod(5000L);
+//    persistenceAdapter.setCreateTablesOnStartup(true);
+//    persistenceAdapter.setAdapter(new MySqlJDBCAdapter());
+//    return persistenceAdapter;
+//  }
 
   @Bean
   public ConnectionFactory connectionFactory() {
     ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
     connectionFactory.setBrokerURL(BROKER_URL);
 
+    // This is overriding redelivery policy by client.
 //    RedeliveryPolicyMap redeliveryPolicyMap = connectionFactory.getRedeliveryPolicyMap();
 //    RedeliveryPolicy redeliveryPolicy = new RedeliveryPolicy();
     RedeliveryPolicy redeliveryPolicy = connectionFactory.getRedeliveryPolicy();
